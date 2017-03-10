274d945a7c16f936d8ba748b6fe36bf563a93244
Beverley Newing <beverley.newing@mac-C02SX8RHGTFL.local>
2017-03-10 23:41:58 +0000

    Addition of sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
 	return gulp.src('./styles/components/main.scss')
+	.pipe(sourcemaps.init())
 	.pipe(sass().on('error', sass.logError))
+	.pipe(sourcemaps.write())
 	.pipe(gulp.dest('./styles'));
 });
 
